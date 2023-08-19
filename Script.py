d76c1f1a72bf130b0cb7d16d103c8caabe5a4027
Nikola Vukojevic <117970622+nikolavukojevic-nv@users.noreply.github.com>
2023-08-19 19:27:06 +0200

    Update Script.py

--- Script.py
+++ Script.py
@@ -2,11 +2,10 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Load the data
-@st.cache
+@st.cache_resource
 def load_data():
     data = pd.read_csv('nv-training-data 2.csv', delimiter=';')
-    data['datetime'] = pd.to_datetime(data['datetime'])
+    data['datetime'] = pd.to_datetime(data['datetime'], format='%Y-%m-%d %H:%M')
     return data
 
 data = load_data()
@@ -14,13 +13,23 @@ data = load_data()
 # Title
 st.title("Exercise Analysis Dashboard")
 
-# Date Range Picker
-start_date, end_date = st.date_input('Pick a date range', [data['datetime'].min(), data['datetime'].max()])
+# Date Range Picker with corrected date range
+start_date, end_date = st.date_input('Pick a date range', [data['datetime'].min().date(), data['datetime'].max().date()])
 filtered_data = data[(data['datetime'] >= pd.Timestamp(start_date)) & (data['datetime'] <= pd.Timestamp(end_date))]
+
+# Handling the SettingWithCopyWarning by creating an explicit copy
+filtered_data = filtered_data.copy()
+
+# Replace ',' with '.' in the 'weight' column and convert to numeric type
+filtered_data['weight'] = pd.to_numeric(filtered_data['weight'].str.replace(',', '.'), errors='coerce')
+filtered_data['reps'] = pd.to_numeric(filtered_data['reps'], errors='coerce')
+filtered_data['sets'] = pd.to_numeric(filtered_data['sets'], errors='coerce')
 
 # Calculate total and adjusted total work
 filtered_data['total_work'] = filtered_data['weight'] * filtered_data['reps'] * filtered_data['sets']
 filtered_data['adjusted_total_work'] = filtered_data['reps'] * filtered_data['sets']
+
+# Summarize total and adjusted total work
 summary = filtered_data.groupby('exercise').agg({
     'total_work': 'sum',
     'adjusted_total_work': 'sum'
